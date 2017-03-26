4b30dbc92be7b6e3bc99aa5e9cbd153977eb37a4
Iiro <iiro.peltola321@gmail.com>
2017-03-26 09:10:21 +0000

    test

--- hello.py
+++ hello.py
@@ -1,13 +1,15 @@
 import os
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello world!'
+@app.route('/login', methods=['GET'])
+def login():
+    if request.values:
+        return "username is " + request.values['username']
+    else:
+        return '<form method="get" action="/login"><input type="text" name="username" /><p><button type="submit">Submit</button></form>'
 
 if __name__ == '__main__':
-    host = os.getenv('IP', '0.0.0.0')
-    port = int(os.getenv('PORT', 5000))
-    app.run(host=host, port=port)
+    app.debug = True
+    app.run(app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 5000))))
